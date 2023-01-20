d4a2935757836145687f19815e228ccd146481bd
Akira Nakamura <s.com.baseball.0209@gmail.com>
2023-01-20 09:13:38 +0900

    add practice

---
+++ BaseBall07/src/main/java/com/example/demo/BaseBall07.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class BaseBall07 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public String test1(String str) {
+		return str;
+	}
+	
+	public String test2(String str) {
+		str = "値変更";
+		return str;
+	}
+	
+	public int calc3times(int num) {
+		num = num * 3;
+		return num;
+	}
+ 
+}
